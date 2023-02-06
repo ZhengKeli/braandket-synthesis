7fa8f25dce201f855de661cc1a5e03cba0db88d9
keli <zhengkeli2009@126.com>
2023-02-06 13:21:37 +0800

    exposed more classes and values from package "operation"

--- src/qsythesis/operation/__init__.py
+++ src/qsythesis/operation/__init__.py
@@ -1,1 +1,4 @@
-from .operation import QOperation, QOperationTrait, SE, Op, Tr
+from .matrix import MatrixOperation, QubitsMatrixOperation
+from .operation import Op, QOperation, QOperationTrait, SE, Tr
+from .structural import ControlledOperation, RemappedOperation, SequentialOperation
+from .traits import ApplyOnMixedStateTrait, ApplyOnPureStateTrait, KetSpaces, KrausTrait, TensorTrait
