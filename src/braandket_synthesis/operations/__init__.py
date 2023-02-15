e9e0f10c497531238e6454cd77730f30917e3831
keli <zhengkeli2009@126.com>
2023-02-15 10:35:23 +0800

    added definitions of common used quantum gates

--- src/braandket_synthesis/operations/__init__.py
+++ src/braandket_synthesis/operations/__init__.py
@@ -1,2 +1,4 @@
+from .gates import CX, CY, CZ, H, NOT, Rx, Ry, Rz, S, T, X, Y, Z
+from .gates import QuantumGate, QuantumGateWithParam
 from .matrix import MatrixOperation, QubitsMatrixOperation
 from .structural import ControlledOperation, RemappedOperation, SequentialOperation
