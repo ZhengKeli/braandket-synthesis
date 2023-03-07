eb08aa7d67192102f59df8f31d34d6b4ee25dbe7
keli <zhengkeli2009@126.com>
2023-03-07 14:09:23 +0800

    added ControlledApply

--- src/braandket_synthesis/operations/structural/controlled.py
+++ src/braandket_synthesis/operations/structural/controlled.py
@@ -1,8 +1,8 @@
 from typing import Generic, Iterable, Optional, Union
 
-from braandket import Backend, OperatorTensor, prod, sum
+from braandket import Backend, MixedStateTensor, OperatorTensor, PureStateTensor, prod, sum
 from braandket_synthesis.basics import Op, QOperation
-from braandket_synthesis.traits import KetSpaces, ToTensor
+from braandket_synthesis.traits import Apply, KetSpaces, ToTensor
 from braandket_synthesis.utils import iter_structured, iter_structured_zip
 
 
@@ -24,6 +24,36 @@ class Controlled(QOperation, Generic[Op]):
     @property
     def keys(self) -> Union[int, Iterable]:
         return self._keys
+
+
+class ControlledApply(Apply[Controlled]):
+    def apply_on_state_tensor(self,
+            spaces: KetSpaces,
+            tensor: Union[PureStateTensor, MixedStateTensor]
+    ) -> Union[PureStateTensor, MixedStateTensor]:
+        control_spaces, target_spaces = spaces
+
+        control_i_tensor = prod(*(
+            sp.identity() for sp in iter_structured(control_spaces)))
+        control_on_tensor = prod(*(
+            sp.projector(k) for sp, k in iter_structured_zip(control_spaces, self.operation.keys)))
+        control_off_tensor = control_i_tensor - control_on_tensor
+
+        target_on_tensor = self.operation.bullet.trait(Apply).apply_on_state_tensor(target_spaces, tensor)
+        target_off_tensor = tensor
+
+        if isinstance(tensor, PureStateTensor):
+            return PureStateTensor.of(sum(
+                control_on_tensor @ target_on_tensor,
+                control_off_tensor @ target_off_tensor
+            ))
+        elif isinstance(tensor, MixedStateTensor):
+            return MixedStateTensor.of(sum(
+                control_on_tensor @ target_on_tensor @ control_on_tensor,
+                control_off_tensor @ target_off_tensor @ control_off_tensor
+            ))
+        else:
+            raise TypeError(f"Expected type of tensor PureStateTensor or MixedStateTensor, got {type(tensor)}!")
 
 
 class ControlledToTensor(ToTensor[Controlled]):
