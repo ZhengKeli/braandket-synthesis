2c77d9f162efbfc974b84305a752ed46d1c79a6a
keli <zhengkeli2009@126.com>
2023-03-07 14:33:11 +0800

    fixed the missing "CNOT" in package braandket_synthesis.operations

--- src/braandket_synthesis/operations/__init__.py
+++ src/braandket_synthesis/operations/__init__.py
@@ -1,4 +1,4 @@
-from .gates import CX, CY, CZ, H, NOT, Rx, Ry, Rz, S, T, X, Y, Z
+from .gates import CNOT, CX, CY, CZ, H, NOT, Rx, Ry, Rz, S, T, X, Y, Z
 from .gates import QuantumGate, QuantumGateWithParam
 from .numeric import *
 from .structural import *
